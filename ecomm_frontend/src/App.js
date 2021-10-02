6745da1000a7684ce640a6d4ec5a98a64c0e4397
Nikhil Sachan <nsachan1983@gmail.com>
2021-10-02 23:12:27 +0530

    navbar created

--- ecomm_frontend/src/App.js
+++ ecomm_frontend/src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Error from "./components/Error";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Ecommerce App</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

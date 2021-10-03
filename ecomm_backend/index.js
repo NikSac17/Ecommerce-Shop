785bd3b7a432410a3a51a6963f456dfdf5b60dbe
Nikhil Sachan <nsachan1983@gmail.com>
2021-10-03 18:36:43 +0530

    models created

--- ecomm_backend/index.js
+++ ecomm_backend/index.js
@@ -1,1 +1,27 @@
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = 5000;
+
+//mongo connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Database down...");
+    console.log(err);
+  });
+
+app.use(express.json()); //to pass json object in req body
+
+//routes
+app.use("/api/user", require("./routes/user"));
+
+app.listen(port, () => {
+  console.log("Backend server running...");
+});

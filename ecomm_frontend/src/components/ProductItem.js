7ac9bab96270d9f2d8f0bb852e4de0c42c255207
Nikhil Sachan <nsachan1983@gmail.com>
2021-11-05 18:40:42 +0530

    minor frontend changes

--- ecomm_frontend/src/components/ProductItem.js
+++ ecomm_frontend/src/components/ProductItem.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+} from "@material-ui/core";
 
-const ProductItem = ({item}) => {
-    return (
-        <div>
-            <h1>{item.title}</h1>
-        </div>
-    )
-}
+const ProductItem = ({ item }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="30"
+          width="30"
+          image={item.image}
+          alt={item.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
--- ecomm_frontend/src/pages/About.js
+++ ecomm_frontend/src/pages/About.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import aboutImage from "../images/about.jpeg"
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <img src={aboutImage}/>
         </div>
     )
 }

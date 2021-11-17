ef09a5f99ec489e2517138b5791d801454296d4b
Nikhil Sachan <nsachan1983@gmail.com>
2021-11-17 09:54:31 +0530

    cart api changes

--- ecomm_backend/models/Cart.js
+++ ecomm_backend/models/Cart.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
     image: {
       type: String,
       required: true,
--- ecomm_backend/routes/cart.js
+++ ecomm_backend/routes/cart.js
@@ -1,12 +1,20 @@
 const router = require("express").Router();
+const fetchUser = require("../middleware/fetchUser");
 const Cart = require("../models/Cart");
 
 //add Item
-router.post("/addToCart", async (req, res) => {
+router.post("/addToCart", fetchUser, async (req, res) => {
   try {
     const { image, title, price, quantity, subtotal } = req.body;
 
-    const cart = new Cart({ image, title, price, quantity, subtotal });
+    const cart = new Cart({
+      image,
+      title,
+      price,
+      quantity,
+      subtotal,
+      user: req.user.id,
+    });
     const saveCart = await cart.save();
     res.send(saveCart);
   } catch (error) {
@@ -16,10 +24,30 @@ router.post("/addToCart", async (req, res) => {
 });
 
 //fetch from cart
-router.get("/getFromCart", async (req, res) => {
+router.get("/getFromCart", fetchUser, async (req, res) => {
   try {
-      const cart = await Cart.find({});
-      res.send(cart);
+    const cart = await Cart.find({ user: req.user.id });
+    res.send(cart);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//delete from cart
+router.delete("/deleteFromCart/:id", fetchUser, async (req, res) => {
+  try {
+    let cart = await Cart.findById(req.params.id);
+    if (!cart) {
+      res.status(404).send("Not found");
+    }
+
+    if (cart.user.toString() !== req.user.id) {
+      res.status(404).send("Not Allowed");
+    }
+
+    cart = await Cart.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Deleted" });
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Internal Server Error");

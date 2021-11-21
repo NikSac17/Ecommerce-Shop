5243c727d611020a6b823a6d7e252166328057f2
Nikhil Sachan <nsachan1983@gmail.com>
2021-11-21 10:46:55 +0530

    minor changes

--- ecomm_frontend/src/pages/About.js
+++ ecomm_frontend/src/pages/About.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import aboutImage from "../images/about.jpeg"
+import React from "react";
+import aboutImage from "../images/about.jpeg";
 
 const About = () => {
-    return (
-        <div>
-            <img src={aboutImage}/>
+  return (
+    <>
+      <div className="container my-5">
+        <div className="row featurette d-flex justify-content-center align-items-center">
+          <div className="col-md-5">
+            <img
+              className="img-fluid"
+              data-src="holder.js/400x400/auto"
+              alt="400x400"
+              src={aboutImage}
+              data-holder-rendered="true"
+            />
+          </div>
+          <div className="col-md-7 ">
+            <h1 className="featurette-heading">Our Story</h1>
+            <p className="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+              atque ea autem soluta ducimus dolores maiores saepe. Iure magnam
+              iusto architecto ad provident pariatur quaerat asperiores,
+              suscipit nam nulla quo consectetur ab minima dolorum doloribus,
+              fuga minus cupiditate. Est dolore consequuntur earum blanditiis
+              doloribus debitis molestias consectetur quod quaerat quas?
+            </p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;

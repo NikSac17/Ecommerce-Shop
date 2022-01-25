663483e118c6c1e76b3a891eb7d2a2e3777329fd
Nikhil Sachan <nsachan1983@gmail.com>
2022-01-25 22:54:18 +0530

    product changes

--- ecomm_frontend/src/pages/Products.js
+++ ecomm_frontend/src/pages/Products.js
@@ -2,40 +2,53 @@ import React, { useEffect, useState } from "react";
 import ProductItem from "../components/ProductItem";
 import Loading from "../components/Loading";
 
-const END_POINT="http://localhost:5000";
+const END_POINT = "http://localhost:5000";
 
 const Products = () => {
+  const [data, setData] = useState([]);
   var [products, setProducts] = useState([]);
 
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
 
   const getProducts = async () => {
-    const response = await fetch(
-      `${END_POINT}/api/products/getAllItems`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await fetch(`${END_POINT}/api/products/getAllItems`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     const json = await response.json();
     setLoading(false);
     setProducts(json);
-    console.log(products);
+    console.log("prod", products);
+  };
+
+  const getData = async () => {
+    const response = await fetch(`${END_POINT}/api/products/getAllItems`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+    setLoading(false);
+    setData(json);
+    console.log("data", data);
   };
 
   useEffect(() => {
     getProducts();
+    getData();
     console.log(products);
   }, []);
 
   const categories = [
     "All",
     ...new Set(
-      products.map((item) => {
+      data.map((item) => {
         return item.category;
       })
     ),
@@ -44,7 +57,7 @@ const Products = () => {
   const brands = [
     "All",
     ...new Set(
-      products.map((item) => {
+      data.map((item) => {
         return item.brand;
       })
     ),
@@ -54,29 +67,37 @@ const Products = () => {
     e.preventDefault();
   };
 
-  const filterCategory=(category)=>{
-     console.log(category);
-     products=products.filter(product=>product.category===category);
-     console.log(products,category);
-     return products;
-  }
+  const filterCategory = (category) => {
+    console.log(category);
+    setProducts(products.filter((product) => product.category === category));
+    console.log(products, category);
+    if (category === "All") {
+      getProducts();
+    }
+  };
 
-  const filterBrand=(brand)=>{
+  const filterBrand = (brand) => {
     console.log(brand);
-  }
+    setProducts(products.filter((product) => product.brand === brand));
+    console.log(products, brand);
+    if (brand === "All") {
+      getProducts();
+    }
+  };
 
-  const clearFilter=()=>{
-    
-  }
+  const clearFilter = () => {
+    getProducts();
+    alert("Filters Removed");
+  };
 
   return (
     <div className="d-flex bd-highlight">
       <div className="p-2 flex-shrink-1 bd-highlight my-4">
         <form onSubmit={handleSubmit}>
           <textarea
-            className="form-control"
+            className="form-control "
             id="exampleFormControlTextarea1"
-            placeholder="Search"
+            placeholder=""
             rows="1"
             name="name"
             value={search}
@@ -88,7 +109,12 @@ const Products = () => {
           {categories.map((category) => {
             return (
               <div>
-                <button className="btn" onClick={()=>filterCategory(category)}>{category}</button>
+                <button
+                  className="btn"
+                  onClick={() => filterCategory(category)}
+                >
+                  {category}
+                </button>
               </div>
             );
           })}
@@ -99,12 +125,16 @@ const Products = () => {
           {brands.map((brand) => {
             return (
               <div>
-                <button className="btn" onClick={()=>filterBrand(brand)}>{brand}</button>
+                <button className="btn" onClick={() => filterBrand(brand)}>
+                  {brand}
+                </button>
               </div>
             );
           })}
         </div>
-        <button className="btn btn-danger" onClick={()=>clearFilter()}>Clear Filters</button>
+        <button className="btn btn-danger" onClick={() => clearFilter()}>
+          Clear Filters
+        </button>
       </div>
       <div className="p-2 w-100 bd-highlight">
         <div className="row my-3">
